09b57aca1b3b2d25e030f11761d69953db6916a6
Rim Bose <rimbose123@gmail.com>
2026-07-24 11:29:49 +0530

    feat: add react-icons dependency and update App component to use Landing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Landing from "./pages/Landing/Landing"
 
 function App() {
   return (
-    <div className="text-blue-300 bg-gray-900 h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold underline">
-        tailwind is working!
-      </h1>
-    </div>
+  <>
+  <Landing/>
+  </>
   )
 }
 
